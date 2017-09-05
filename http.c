58dcb39d55f282136d8eca3049bf5a43ae5c374a
jason886 <359152155@qq.com>
2017-09-05 15:39:18 +0800

    modify

--- http.c
+++ http.c
@@ -34,54 +34,47 @@
 
 #ifdef WIN32
 static int inet_pton(int af, const char *src, void *dst) {
-  struct sockaddr_storage ss;
-  int size = sizeof(ss);
-  char src_copy[INET6_ADDRSTRLEN+1];
+    struct sockaddr_storage ss;
+    int size = sizeof(ss);
+    char src_copy[INET6_ADDRSTRLEN+1];
+    
+    memset(&ss, 0x00, sizeof(ss));
+    /* stupid non-const API */
+    strncpy (src_copy, src, INET6_ADDRSTRLEN+1);
+    src_copy[INET6_ADDRSTRLEN] = 0;
 
-  memset(&ss, 0x00, sizeof(ss));
-  /* stupid non-const API */
-  strncpy (src_copy, src, INET6_ADDRSTRLEN+1);
-  src_copy[INET6_ADDRSTRLEN] = 0;
-  printf("src_copy = %s\n", src_copy);
-
-  int ret = WSAStringToAddress(src_copy, af, NULL, (struct sockaddr *)&ss, &size);
-  printf("ret = %d\n", ret);
-  if (WSAStringToAddress(src_copy, af, NULL, (struct sockaddr *)&ss, &size) == 0) {
-      printf("af = %d\n");
-      switch(af) {
-      case AF_INET:
-          *(struct in_addr *)dst = ((struct sockaddr_in *)&ss)->sin_addr;
-          return 1;
-      case AF_INET6:
-          *(struct in6_addr *)dst = ((struct sockaddr_in6 *)&ss)->sin6_addr;
-          return 1;
-      }
-  }
-  int err = WSAGetLastError();
-  printf("err = %d\n", err);
-  printf("return 0\n");
-  return 0;
+    if (WSAStringToAddress(src_copy, af, NULL, (struct sockaddr *)&ss, &size) == 0) {
+        switch(af) {
+        case AF_INET:
+            *(struct in_addr *)dst = ((struct sockaddr_in *)&ss)->sin_addr;
+            return 1;
+        case AF_INET6:
+            *(struct in6_addr *)dst = ((struct sockaddr_in6 *)&ss)->sin6_addr;
+            return 1;
+        }
+    }
+    return 0;
 }
 
 static const char *inet_ntop(int af, const void *src, char *dst, socklen_t size) {
-  struct sockaddr_storage ss;
-  unsigned long s = size;
-
-  memset(&ss, 0x00, sizeof(ss));
-  ss.ss_family = af;
-
-  switch(af) {
+    struct sockaddr_storage ss;
+    unsigned long s = size;
+    
+    memset(&ss, 0x00, sizeof(ss));
+    ss.ss_family = af;
+    
+    switch(af) {
     case AF_INET:
-      ((struct sockaddr_in *)&ss)->sin_addr = *(struct in_addr *)src;
-      break;
+        ((struct sockaddr_in *)&ss)->sin_addr = *(struct in_addr *)src;
+        break;
     case AF_INET6:
-      ((struct sockaddr_in6 *)&ss)->sin6_addr = *(struct in6_addr *)src;
-      break;
+        ((struct sockaddr_in6 *)&ss)->sin6_addr = *(struct in6_addr *)src;
+        break;
     default:
-      return NULL;
-  }
-  /* cannot direclty use &size because of strict aliasing rules */
-  return (WSAAddressToString((struct sockaddr *)&ss, sizeof(ss), NULL, dst, &s) == 0)?
+        return NULL;
+    }
+    /* cannot direclty use &size because of strict aliasing rules */
+    return (WSAAddressToString((struct sockaddr *)&ss, sizeof(ss), NULL, dst, &s) == 0)?
           dst : NULL;
 }
 #endif
