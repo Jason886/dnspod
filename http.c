eae8f88cecc7f669a5b066f01372752436421932
jason886 <359152155@qq.com>
2017-09-05 15:11:03 +0800

    modify

--- http.c
+++ http.c
@@ -38,21 +38,23 @@ static int inet_pton(int af, const char *src, void *dst) {
   int size = sizeof(ss);
   char src_copy[INET6_ADDRSTRLEN+1];
 
-  ZeroMemory(&ss, sizeof(ss));
+  memset(&ss, 0x00, sizeof(ss));
   /* stupid non-const API */
   strncpy (src_copy, src, INET6_ADDRSTRLEN+1);
   src_copy[INET6_ADDRSTRLEN] = 0;
 
   if (WSAStringToAddress(src_copy, af, NULL, (struct sockaddr *)&ss, &size) == 0) {
-    switch(af) {
+      printf("af = %d\n");
+      switch(af) {
       case AF_INET:
-    *(struct in_addr *)dst = ((struct sockaddr_in *)&ss)->sin_addr;
-    return 1;
+          *(struct in_addr *)dst = ((struct sockaddr_in *)&ss)->sin_addr;
+          return 1;
       case AF_INET6:
-    *(struct in6_addr *)dst = ((struct sockaddr_in6 *)&ss)->sin6_addr;
-    return 1;
-    }
+          *(struct in6_addr *)dst = ((struct sockaddr_in6 *)&ss)->sin6_addr;
+          return 1;
+      }
   }
+  printf("return 0\n");
   return 0;
 }
 
